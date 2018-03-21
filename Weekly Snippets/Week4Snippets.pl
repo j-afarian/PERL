af3999a5528a5743bcbd2880f742909dc89b2414
Jenna Afarian <jenna.afarian@hotmail.com>
2018-03-22 07:54:43 +1000

    Update w4 snippets

--- Weekly Snippets/Week4Snippets.pl
+++ Weekly Snippets/Week4Snippets.pl
@@ -5,7 +5,9 @@
 # learning modules and text books.
 
 ############### REGULAR EXPRESSIONS ###############
-# -------------- PATTERN MATCHING ---------------
+###################################################
+#
+# ############### PATTERN MATCHING ################
 # The basic syntax of pattern matching is: /pattern/
 # Patterns can be bound to another expression using the
 # binding operators =~ and !~ (contains, does not contain).
@@ -105,7 +107,7 @@ $_ = "DATA 1 2 3 4";
 s/\d+/$&*3/ge if /^DATA/;
 print;
 
-##################### TRANSLATION ######################
+##################### TRANSLATION #######################
 # Basic syntax is tr/<search list>/<replacement list>/
 # Translation operator scans through the string bound by =~,
 # or $_, or character by character, and replaces a character in
@@ -278,3 +280,125 @@ print "--$`--$&--$'--\n" if /.*?/;
 # If the string matches with any of these sub-patterns, then matched
 # substrings are stored in a set of special variables.
 # $1 for first parenthesis, $2 for second pair, so on
+
+$_ = "Larry has a little little little turtle";
+my ($who, $what) = /^(\w+).*(\b\w+)$/;
+print "$who has $what.\n";
+
+# (\w+) matches with Larry and stored in $1,
+# (\b\w+) matches with turtle stored in $2
+
+$_ = "Will Jackson,987767";
+s/(\D+),(\d+)/$2,$1/;
+print "$_\n";
+
+# Inside the pattern, the sub-pattern matches can be referred to
+# as \1, \2, \3 etc.
+
+$_ = "Larry has a little little little pony";
+print qq/Found repeating word "$1"\n/ if /(\w+) \1/;
+
+####################### FORMATS ########################
+########################################################
+
+# Informational only, not assessable
+
+#printf and sprintf provide more formatting controls.
+$course = "Java 1";
+$pass = 74;
+$total = 92;
+$_ = sprintf "Pass rate of %s is %.2f\%\n", $course, $pass/$total * 100;
+print;
+
+# Another pre-formatted string method is "here-document"
+$student = "Jack Smith";
+$mark = 90;
+
+print<<EOF;
+Student Name: $student
+Mark: $mark
+EOF
+
+# Formats can control output layout and keep track of number
+# of lines, page number, page headers etc.
+# FORMAT: to declare a format
+# WRITE: ouput using the defined format
+
+# -------------------- DECLARATION --------------------
+# Basic syntax of a format declaration is:
+format FORMATNAME = 
+<body>
+.
+
+# If there is no name, then the format will be applied
+# to STDOUT which is the default.
+# The body can contain:
+# - Picture lines: each one defines the format for one line
+# - Arguement lines: each supplies values to the picture line above
+# - Comments: start with #
+
+# values to be supplied are marked with @ or ^. They are subsitutions.
+format =
+Price: $@<<<< Quantity: @ units
+$price, $quantity
+.
+
+($price, $quantity) = (150, 3);
+#write;
+
+# Each format can have a TOP defined which is a header,
+# automatically included when you use the format:
+# FORMATNAME_TOP
+
+# --------------- SUBSTITUTION FIELDS -----------------
+# @<<<< defines 5 char left aligned field (@ counts as 1 char).
+# @||| defines a 4 char center aligned field.
+# @>> defines a 3 char right aligned field.
+# @#.## the numeric value will display in a 5 character field
+# with 2 digits after the decimal point.
+
+my ($price, $quantity);
+
+open my $file, "data.txt" or die "Invalid File";
+
+$~ = "TABLE";         # use format TABLE
+$^ = "TABLE_TOP";    # header
+
+
+while(<$file>) {
+   ($price, $quantity) = split;
+   write;
+}
+
+format TABLE_TOP =
+Item | Price | Quantity | Total
+-------------------------------
+.
+
+format TABLE = 
+@>>> | @|||| | @<<<<<<< | @###.##
+$., $price, $quantity, $price * $quantity
+.
+
+# $~ is the current format in use
+# $^ is the current format header
+# $. is the current number in the most recent filehandle
+
+# ------------------- CARETS ------------------------
+# Carets (^) are similar to @ for field filling, but are substituted
+# with a scalar variable, not an arbitrary expression.
+# It reads and modifies the variable - characters filled in the
+# substitution field is removed from the variable
+format STDOUT = 
+^|||||||||||
+$a
+.
+
+$~ = "STDOUT";
+$^ = "STDOUT_TOP";
+
+$a = "I'm a little teapot short and stout";
+write; # "I'm a little"
+print $a; # "teapot short and stout"
+
+# Shrinking is useful for printing a long string over multiple lines.
